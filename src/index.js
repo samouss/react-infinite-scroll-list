bf9e3059c9bfc1fddabea12ce5559d7594554d92
Samuel Vaillant <samuel.vllnt@gmail.com>
2016-09-16 17:44:07 +0200

    feat(InfiniteList): add component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,65 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Container from './Container';
-import './index.css';
+import React, { PropTypes } from 'react';
 
-ReactDOM.render(
-  <Container />,
-  document.getElementById('root')
-);
+/**
+ * @name   InfiniteList
+ * @desc   Display given children nodes and attach scroll event on <ul> for handle reload on scroll
+ * @return {ReactElement}
+ */
+export default function InfiniteList(props) {
+  const {
+    containerHeight,
+    isLoading,
+    isEndReach,
+    onThresholdReach,
+    children,
+    className = 'infinite-list',
+    threshold = 0,
+  } = props;
+
+  return (
+    <ul
+      className={className}
+      style={{ height: containerHeight, 'overflow-y': 'scroll' }}
+      onScroll={onScroll(isLoading, isEndReach, threshold, onThresholdReach)}
+    >
+      {children}
+    </ul>
+  );
+}
+
+/**
+ * @name   onScroll
+ * @desc   Takes given props for determine then return function who takes event and determine if we need to execute
+ *         callback for load more content
+ * @param  {boolean}                      isLoading
+ * @param  {boolean}                      isEndReach
+ * @param  {number}                       threshold
+ * @param  {Function}                     callback
+ * @return {(event: ScrollEvent) => void}
+ */
+function onScroll(isLoading, isEndReach, threshold, callback) {
+  return (event) => {
+    if (isLoading || isEndReach) {
+      return;
+    }
+
+    const { target } = event;
+
+    const scrollPosition = target.clientHeight + target.scrollTop;
+    const scrollThreshold = Math.ceil(target.scrollHeight - threshold);
+
+    if (scrollPosition >= scrollThreshold) {
+      callback(event);
+    }
+  };
+}
+
+InfiniteList.propTypes = {
+  containerHeight: PropTypes.string.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  isEndReach: PropTypes.bool.isRequired,
+  onThresholdReach: PropTypes.func.isRequired,
+  children: PropTypes.node,
+  className: PropTypes.string,
+  threshold: PropTypes.number,
+};
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,34 +1,246 @@
+import React from 'react';
 import chai from 'chai';
 import sinon from 'sinon';
-import React from 'react';
+import { shallow } from 'enzyme';
+import InfiniteList from './index';
 
 chai.should();
 
-describe('sample', () => {
-  it('should pass', () => {
-    const expectation = true;
-    const result = true;
-
-    result.should.be.equal(expectation);
-  });
-
-  it('should pass too', () => {
-    const spy = sinon.spy();
-
-    spy();
-
-    spy.calledOnce.should.be.equal(true);
-  });
-
-  it('should pass with JSX', () => {
-    const component = (
-      <div>Should contain test</div>
-    );
+describe('<InfiniteList />', () => {
+  it('should render the component correctly with default props', () => {
+    const onThresholdReach = () => {};
 
     const expectation = (
-      <div>Should contain test</div>
+      <ul
+        className="infinite-list"
+        style={{ height: '750px', 'overflow-y': 'scroll' }}
+      >
+        <li>Child 1</li>
+        <li>Child 2</li>
+        <li>Child 3</li>
+      </ul>
     );
 
-    component.should.be.deep.equal(expectation);
+    const wrapper = shallow(
+      <InfiniteList
+        containerHeight="750px"
+        isLoading={false}
+        isEndReach={false}
+        onThresholdReach={onThresholdReach}
+      >
+        <li>Child 1</li>
+        <li>Child 2</li>
+        <li>Child 3</li>
+      </InfiniteList>
+    );
+
+    wrapper.containsMatchingElement(expectation).should.be.equal(true);
+  });
+
+  it('should render the component correctly with given props', () => {
+    const onThresholdReach = () => {};
+
+    const expectation = (
+      <ul
+        className="custom-infinite-list"
+        style={{ height: '750px', 'overflow-y': 'scroll' }}
+      >
+        <li>Child 1</li>
+        <li>Child 2</li>
+        <li>Child 3</li>
+      </ul>
+    );
+
+    const wrapper = shallow(
+      <InfiniteList
+        className="custom-infinite-list"
+        containerHeight="750px"
+        isLoading={false}
+        isEndReach={false}
+        onThresholdReach={onThresholdReach}
+      >
+        <li>Child 1</li>
+        <li>Child 2</li>
+        <li>Child 3</li>
+      </InfiniteList>
+    );
+
+    wrapper.containsMatchingElement(expectation).should.be.equal(true);
+  });
+
+  it('should trigger onThresholdReach callback on scroll if default threshold is reach', () => {
+    const onThresholdReach = sinon.spy();
+
+    const wrapper = shallow(
+      <InfiniteList
+        containerHeight="750px"
+        isLoading={false}
+        isEndReach={false}
+        onThresholdReach={onThresholdReach}
+      >
+        <li>Child 1</li>
+        <li>Child 2</li>
+        <li>Child 3</li>
+      </InfiniteList>
+    );
+
+    wrapper
+      .find('ul')
+      .simulate('scroll', {
+        target: {
+          clientHeight: 750,
+          scrollHeight: 1000,
+          scrollTop: 250,
+        },
+      });
+
+    onThresholdReach.calledOnce.should.be.equal(true);
+  });
+
+  it('should trigger onThresholdReach callback on scroll if given threshold is reach', () => {
+    const onThresholdReach = sinon.spy();
+
+    const wrapper = shallow(
+      <InfiniteList
+        containerHeight="750px"
+        isLoading={false}
+        isEndReach={false}
+        onThresholdReach={onThresholdReach}
+        threshold={100}
+      >
+        <li>Child 1</li>
+        <li>Child 2</li>
+        <li>Child 3</li>
+      </InfiniteList>
+    );
+
+    wrapper
+      .find('ul')
+      .simulate('scroll', {
+        target: {
+          clientHeight: 750,
+          scrollHeight: 1000,
+          scrollTop: 150,
+        },
+      });
+
+    onThresholdReach.calledOnce.should.be.equal(true);
+  });
+
+  it('should not trigger onThresholdReach callback on scroll if default threshold is not reach', () => {
+    const onThresholdReach = sinon.spy();
+
+    const wrapper = shallow(
+      <InfiniteList
+        containerHeight="750px"
+        isLoading={false}
+        isEndReach={false}
+        onThresholdReach={onThresholdReach}
+      >
+        <li>Child 1</li>
+        <li>Child 2</li>
+        <li>Child 3</li>
+      </InfiniteList>
+    );
+
+    wrapper
+      .find('ul')
+      .simulate('scroll', {
+        target: {
+          clientHeight: 750,
+          scrollHeight: 1000,
+          scrollTop: 200,
+        },
+      });
+
+    onThresholdReach.calledOnce.should.be.equal(false);
+  });
+
+  it('should not trigger onThresholdReach callback on scroll if given threshold is not reach', () => {
+    const onThresholdReach = sinon.spy();
+
+    const wrapper = shallow(
+      <InfiniteList
+        containerHeight="750px"
+        isLoading={false}
+        isEndReach={false}
+        onThresholdReach={onThresholdReach}
+        threshold={100}
+      >
+        <li>Child 1</li>
+        <li>Child 2</li>
+        <li>Child 3</li>
+      </InfiniteList>
+    );
+
+    wrapper
+      .find('ul')
+      .simulate('scroll', {
+        target: {
+          clientHeight: 750,
+          scrollHeight: 1000,
+          scrollTop: 100,
+        },
+      });
+
+    onThresholdReach.calledOnce.should.be.equal(false);
+  });
+
+  it('should not trigger onThresholdReach callback on scroll if loading is true', () => {
+    const onThresholdReach = sinon.spy();
+
+    const wrapper = shallow(
+      <InfiniteList
+        containerHeight="750px"
+        isLoading
+        isEndReach={false}
+        onThresholdReach={onThresholdReach}
+      >
+        <li>Child 1</li>
+        <li>Child 2</li>
+        <li>Child 3</li>
+      </InfiniteList>
+    );
+
+    wrapper
+      .find('ul')
+      .simulate('scroll', {
+        target: {
+          clientHeight: 750,
+          scrollHeight: 1000,
+          scrollTop: 250,
+        },
+      });
+
+    onThresholdReach.calledOnce.should.be.equal(false);
+  });
+
+  it('should not trigger onThresholdReach callback on scroll if end is reach', () => {
+    const onThresholdReach = sinon.spy();
+
+    const wrapper = shallow(
+      <InfiniteList
+        containerHeight="750px"
+        isLoading={false}
+        isEndReach
+        onThresholdReach={onThresholdReach}
+      >
+        <li>Child 1</li>
+        <li>Child 2</li>
+        <li>Child 3</li>
+      </InfiniteList>
+    );
+
+    wrapper
+      .find('ul')
+      .simulate('scroll', {
+        target: {
+          clientHeight: 750,
+          scrollHeight: 1000,
+          scrollTop: 250,
+        },
+      });
+
+    onThresholdReach.calledOnce.should.be.equal(false);
   });
 });
