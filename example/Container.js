364183e878e6e60cdd13c25972dc1b38294afb0f
Samuel Vaillant <samuel.vllnt@gmail.com>
2016-09-16 19:03:33 +0200

    feat(example): add example with loader

---
+++ example/Container.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import InfiniteList from '../src';
+
+export default class Container extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: false,
+      isEndReach: false,
+      items: [],
+    };
+
+    this.onThresholdReach = this.onThresholdReach.bind(this);
+  }
+
+  componentDidMount() {
+    this.createElement();
+  }
+
+  onThresholdReach() {
+    this.createElement();
+  }
+
+  createElement() {
+    this.setState({ isLoading: true });
+
+    const items = [];
+
+    for (let i = 0; i < 25; i += 1) {
+      const value = this.state.items.length + i;
+
+      items.push({
+        id: value,
+        message: `Placeholder ${value}`,
+      });
+    }
+
+    this.setState({
+      isLoading: false,
+      items: this.state.items.concat(items),
+    });
+  }
+
+  render() {
+    const { isLoading, isEndReach, items } = this.state;
+
+    return (
+      <div>
+        <p className="title-infinite-list">Simple infite list</p>
+
+        <InfiniteList
+          className="custom-infinite-list"
+          containerHeight="648px"
+          isLoading={isLoading}
+          isEndReach={isEndReach}
+          onThresholdReach={this.onThresholdReach}
+          threshold={150}
+        >
+        {items.map(item => (
+          <li
+            className="custom-infinite-list__item"
+            key={item.id}
+          >
+            {item.message}
+          </li>
+        ))}
+        </InfiniteList>
+      </div>
+    );
+  }
+}
---
+++ example/ContainerWithLoader.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import InfiniteList from '../src';
+
+export default class ContainerWithLoader extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: false,
+      isEndReach: false,
+      items: [],
+    };
+
+    this.onThresholdReach = this.onThresholdReach.bind(this);
+  }
+
+  componentDidMount() {
+    this.createElement();
+  }
+
+  onThresholdReach() {
+    this.createElement();
+  }
+
+  createElement() {
+    this.setState({ isLoading: true });
+
+    const items = [];
+
+    for (let i = 0; i < 25; i += 1) {
+      const value = this.state.items.length + i;
+
+      items.push({
+        id: value,
+        message: `Placeholder ${value}`,
+      });
+    }
+
+    setTimeout(() => {
+      this.setState({
+        isLoading: false,
+        items: this.state.items.concat(items),
+      });
+    }, 2500);
+  }
+
+  renderFooter() {
+    if (!this.state.isLoading) {
+      return null;
+    }
+
+    return <li className="custom-infinite-list__item">Loading...</li>;
+  }
+
+  render() {
+    const { isLoading, isEndReach, items } = this.state;
+
+    return (
+      <div>
+        <p className="title-infinite-list">Simple infite list with loader</p>
+
+        <InfiniteList
+          className="custom-infinite-list"
+          containerHeight="648px"
+          isLoading={isLoading}
+          isEndReach={isEndReach}
+          onThresholdReach={this.onThresholdReach}
+          threshold={150}
+        >
+        {items.map(item => (
+          <li
+            className="custom-infinite-list__item"
+            key={item.id}
+          >
+            {item.message}
+          </li>
+        ))}
+        {this.renderFooter()}
+        </InfiniteList>
+      </div>
+    );
+  }
+}
