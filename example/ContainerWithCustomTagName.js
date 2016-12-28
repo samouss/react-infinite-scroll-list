e9d286d97f2715da177f0f0d58f7e00b4dbde3c3
Samuel Vaillant <samuel.vllnt@gmail.com>
2016-12-28 15:01:32 +0100

    feat(exemple): add container tag name

---
+++ example/ContainerWithCustomTagName.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import InfiniteList from '../src';
+
+export default class ContainerWithCustomTagName extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: false,
+      isEndReach: false,
+      items: [],
+    };
+
+    this.createElement = this.createElement.bind(this);
+  }
+
+  componentDidMount() {
+    this.createElement();
+  }
+
+  createElement() {
+    this.setState({ isLoading: true });
+
+    const items = [];
+
+    for (let i = 0; i < 25; i += 1) {
+      const value = this.state.items.length + i;
+
+      items.push({
+        id: value,
+        message: `Placeholder ${value}`,
+      });
+    }
+
+    this.setState({
+      isLoading: false,
+      items: this.state.items.concat(items),
+    });
+  }
+
+  render() {
+    const { isLoading, isEndReach, items } = this.state;
+
+    return (
+      <div>
+        <p className="title-infinite-list">Simple infite list with custom tag</p>
+
+        <InfiniteList
+          className="custom-infinite-list"
+          containerHeight="648px"
+          isLoading={isLoading}
+          isEndReach={isEndReach}
+          containerTagName="ul"
+          threshold={150}
+          onThresholdReach={this.createElement}
+        >
+          {items.map(item => (
+            <li
+              className="custom-infinite-list__item"
+              key={item.id}
+            >
+              {item.message}
+            </li>
+          ))}
+        </InfiniteList>
+      </div>
+    );
+  }
+}
--- example/index.js
+++ example/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import Container from './Container';
 import ContainerWithLoader from './ContainerWithLoader';
+import ContainerWithCustomTagName from './ContainerWithCustomTagName';
 import './index.css';
 
 const App = () => (
   <div>
     <Container />
     <ContainerWithLoader />
+    <ContainerWithCustomTagName />
   </div>
 );
 
