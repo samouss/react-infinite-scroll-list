7a6a852afb6f7bf1b48178ed0c5d8a53fb9a8c9f
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-08-23 10:28:24 +0200

    core: remove useless rollup-plugin-node-resolve plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import path from 'path';
-import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import uglify from 'rollup-plugin-uglify';
@@ -14,7 +13,6 @@ const main = pkg.main;
 const extname = path.extname(pkg.main);
 
 const defaultPlugins = filter([
-  resolve(),
   commonjs(),
   babel({
     exclude: 'node_modules/**',
