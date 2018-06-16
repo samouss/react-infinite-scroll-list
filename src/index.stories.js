b5a5b2ce89851cfd295cc9fbd5a0f4e23b8c6d70
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-06-16 10:07:23 +0200

    feat(storybook): stories

--- src/index.stories.js
+++ src/index.stories.js
@@ -1,23 +1,144 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import InfiniteList from './index';
 
-import { Button, Welcome } from '@storybook/react/demo';
+const createPlaceholderElement = (items = []) =>
+  new Array(25).fill(null).map((_, index) => {
+    const value = items.length + index;
 
-storiesOf('Welcome', module).add('to Storybook', () => (
-  <Welcome showApp={linkTo('Button')} />
-));
+    return {
+      id: value,
+      message: `Placeholder ${value}`,
+    };
+  });
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
+class Container extends Component {
+  static propTypes = {
+    children: PropTypes.func.isRequired,
+    delay: PropTypes.number,
+  };
+
+  static defaultProps = {
+    delay: 0,
+  };
+
+  state = {
+    items: createPlaceholderElement(),
+    isLoading: false,
+    isEndReached: false,
+  };
+
+  onReachThreshold = () => {
+    const { delay } = this.props;
+
+    this.setState({ isLoading: true });
+
+    setTimeout(() => {
+      this.setState(({ items }) => {
+        const next = createPlaceholderElement(items);
+
+        return {
+          isLoading: false,
+          items: items.concat(next),
+        };
+      });
+    }, delay);
+  };
+
+  render() {
+    const { items, isLoading, isEndReached } = this.state;
+
+    return this.props.children({
+      onReachThreshold: this.onReachThreshold,
+      items,
+      isLoading,
+      isEndReached,
+    });
+  }
+}
+
+storiesOf('InfiniteList', module)
+  .add('default', () => (
+    <Container>
+      {({ items, isLoading, isEndReached, onReachThreshold }) => (
+        <InfiniteList
+          containerClassName="InfiniteList InfiniteList--with-height"
+          root="container"
+          isLoading={isLoading}
+          isEndReached={isEndReached}
+          onReachThreshold={onReachThreshold}
+          threshold={150}
+        >
+          {items.map(item => (
+            <div className="InfiniteList__Item" key={item.id}>
+              {item.message}
+            </div>
+          ))}
+        </InfiniteList>
+      )}
+    </Container>
   ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
+  .add('with loader', () => (
+    <Container delay={2500}>
+      {({ items, isLoading, isEndReached, onReachThreshold }) => (
+        <InfiniteList
+          containerClassName="InfiniteList InfiniteList--with-height"
+          root="container"
+          isLoading={isLoading}
+          isEndReached={isEndReached}
+          onReachThreshold={onReachThreshold}
+          threshold={150}
+        >
+          {items.map(item => (
+            <div className="InfiniteList__Item" key={item.id}>
+              {item.message}
+            </div>
+          ))}
+          {isLoading && <li className="InfiniteList__Item">Loading...</li>}
+        </InfiniteList>
+      )}
+    </Container>
+  ))
+  .add('with custom tag name', () => (
+    <Container>
+      {({ items, isLoading, isEndReached, onReachThreshold }) => (
+        <InfiniteList
+          containerClassName="InfiniteList InfiniteList--with-height"
+          root="container"
+          containerTagName="ul"
+          sentinelTagName="li"
+          isLoading={isLoading}
+          isEndReached={isEndReached}
+          onReachThreshold={onReachThreshold}
+          threshold={150}
+        >
+          {items.map(item => (
+            <li className="InfiniteList__Item" key={item.id}>
+              {item.message}
+            </li>
+          ))}
+        </InfiniteList>
+      )}
+    </Container>
+  ))
+  .add('with viewport', () => (
+    <Container>
+      {({ items, isLoading, isEndReached, onReachThreshold }) => (
+        <InfiniteList
+          containerClassName="InfiniteList"
+          root="viewport"
+          isLoading={isLoading}
+          isEndReached={isEndReached}
+          onReachThreshold={onReachThreshold}
+          threshold={350}
+        >
+          {items.map(item => (
+            <div className="InfiniteList__Item" key={item.id}>
+              {item.message}
+            </div>
+          ))}
+        </InfiniteList>
+      )}
+    </Container>
   ));
