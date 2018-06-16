e4bd227d4bf8c024bb574a727f57620ca9952f3b
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-06-16 10:19:49 +0200

    feat: configure storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,11 @@
 import { configure } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
+
+setOptions({
+  name: 'infinite-scroll-list',
+  url: 'https://github.com/samouss/react-infinite-scroll-list',
+  showAddonPanel: false,
+});
 
 const req = require.context('../src', true, /.stories.js$/);
 
